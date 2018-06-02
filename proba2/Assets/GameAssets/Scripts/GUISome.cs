ba2f3ff3c5b976122730305f8055cf955aa742ea
NTFI\xkico <kicoiris@gmail.com>
2018-06-02 12:36:19 +0200

    Final version

---
+++ proba2/Assets/GameAssets/Scripts/GUISome.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GUISome : MonoBehaviour
+{
+    public void Awake()
+    {
+        Cursor.visible = false;
+    }
+  
+    public void Update()
+    {
+        TestScreenCursorLock();
+    }
+
+    private void TestScreenCursorLock()
+    {
+        if (Cursor.lockState != CursorLockMode.Locked && Input.GetMouseButtonDown(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
---
+++ proba2/Assets/MyCraterator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCraterator : MonoBehaviour {
+    public TerrainDeformer deformer;
+    public Transform deformator;
+
+    private void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Ray rayCheck = new Ray(deformator.position, deformator.forward);
+            Debug.DrawRay(rayCheck.origin, rayCheck.direction * 100);
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(rayCheck, out hitInfo, 100))
+            {
+                deformer.DestroyTerrain(hitInfo.point, 4);
+            }
+        }
+    }
+}
